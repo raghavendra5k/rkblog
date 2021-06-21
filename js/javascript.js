df8b2a26434fd2c7b19da6533b76e2ee8dbc9f98
raghavendra5k <83965331+raghavendra5k@users.noreply.github.com>
2021-06-21 23:32:22 +0530

    initial commit on my blog

---
+++ js/javascript.js
@@ -0,0 +1,13 @@
+// window.onload = function myFunction() {
+//     var x = document.getElementById('menu');
+//     console.log(x.style.display);
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//     // for (var i=0;i<x.length;i+=1){
+//     //     x[i].style.display = 'block';
+//     //   }
+//     //alert("Hiiiii")
+//   }
